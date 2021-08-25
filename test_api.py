39433808d84dcdabdbf0d18d58ecf17b590ab7fe
QAfun <arthur.popko@icloud.com>
2021-08-25 13:51:47 +0300

    test country check add

--- test_api.py
+++ test_api.py
@@ -1,4 +1,16 @@
+# -*- encoding=utf8 -*-
+
 import requests
+
+def test_check_country():
+    res = requests.get('http://ip-api.com/json')
+    data = res.json()
+
+    country = data.get('country')
+
+    assert country == 'Belarus', 'Country is wrong!'
+
+
 
 
 
