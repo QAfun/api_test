15af1203476f452d23a4f315f78b0fae25afcef0
QAfun <arthur.popko@icloud.com>
2021-08-25 23:59:18 +0300

    api server added

--- test_api.py
+++ test_api.py
@@ -2,13 +2,16 @@
 
 import requests
 
+url = 'http://ip-api.com/json'
+
 def test_check_country():
-    res = requests.get('http://ip-api.com/json')
+
+    res = requests.get(url)
     data = res.json()
 
     country = data.get('country')
 
-    assert country == 'Litva', 'Country is wrong!'
+    assert country == 'Belarus', 'Country is wrong!'
     
 
 
