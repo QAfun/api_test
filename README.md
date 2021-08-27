a1a46926aaa6a748c3afd27a653fcd9f5807c23f
QAfun <arthur.popko@icloud.com>
2021-08-27 10:29:10 +0300

    clear all

--- README.md
+++
@@ -1,6 +0,0 @@
-# pytest
-cheat sheet:
-git branch
-git checkout main
-git checkout -b feature/TEST-1
-git pull 
--- api_test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-res = requests.get('http://ip-api.com/json')
-
-data = res.json()
-print(data.get('country'))
-print(data.get('query', 'not found'))
-print(data.get('test', 'not found'))
-print(data)
